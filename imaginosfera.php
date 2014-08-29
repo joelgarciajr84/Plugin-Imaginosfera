75dd6e6b4b7891cce27667710235f6a2341c7ce6
Valerio Souza <valeriosza@gmail.com>
2014-08-29 17:03:51 -0300

    add function analytics

--- imaginosfera.php
+++ imaginosfera.php
@@ -46,6 +46,7 @@ add_action(	'wp_logout',					'imaginosfera_logout_redirect');
 add_action(	'admin_bar_menu', 				'imaginosfera_add_toolbar_items', 100);
 //add_action(	'admin_menu', 				'imaginosfera_hide_update_notice');
 add_action(	'admin_footer',					'imaginosfera_posts_status_color');
+add_action( 'wp_footer',                    'imaginosfera_add_googleanalytics');
 add_filter( 'login_headerurl', 				'imaginosfera_login_logo_url' );
 add_filter( 'login_headertitle', 			'imaginosfera_login_logo_url_title' );
 add_filter( 'get_user_option_admin_color', 	'imaginosfera_change_admin_color' ); 
@@ -223,3 +224,16 @@ function imaginosfera_posts_status_color(){
     </style>
 <?php
 }
+
+function imaginosfera_add_googleanalytics() { ?>
+<script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-xxxxxxxx-xx', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+<?php } 
